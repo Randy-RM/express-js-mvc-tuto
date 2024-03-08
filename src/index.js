b833df2e7f1f8ab71afb527075ad769117485d93
Randy-RM <randymuhema@gmail.com>
2024-03-08 15:23:14 +0100

    feat : add cors and dotenv config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express, { json } from "express";
 const app = express();
+const PORT = process.env.PORT || 8000;
+const corsOptions = {
+  origin: `http://localhost:${PORT}`,
+};
+dotenv.config();
 
+// Config
 app.use(json());
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
+// Routes
 app.get("/", (req, res) => {
   res.json({ message: "Hello Crud Node Express" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`The server listens on http://localhost:${PORT}`);
 });
