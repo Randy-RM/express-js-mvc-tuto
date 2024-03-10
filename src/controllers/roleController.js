aabe298ea51e6f00e6dcd4220377d4d330f5743e
Randy-RM <randymuhema@gmail.com>
2024-03-10 13:28:39 +0100

    feat : creat role controller and his basic CRUD function definition

---
+++ src/controllers/roleController.js
@@ -0,0 +1,60 @@
+/*
+--------------------------
+Retrieve all roles from 
+the database.
+--------------------------
+*/
+async function getAllRoles(req, res, next) {
+  return res.send("All roles");
+}
+
+/*
+--------------------------
+Create and save a new role
+in the database
+--------------------------
+*/
+async function createRole(req, res, next) {
+  return res.send("role is Created");
+}
+
+/*
+--------------------------
+Update a role by the id 
+in the request
+--------------------------
+*/
+async function updateRole(req, res, next) {
+  return res.send("role is Updated");
+}
+
+/*
+--------------------------
+Delete a role with 
+the specified id 
+in the request
+--------------------------
+*/
+async function deleteRole(req, res, next) {
+  return res.send("role is deleted");
+}
+
+/*
+--------------------------
+Delete all roles from 
+the database.
+--------------------------
+*/
+async function deleteAllRole(req, res, next) {
+  return res.send("roles are deleted");
+}
+
+export { createRole, deleteAllRole, deleteRole, getAllRoles, updateRole };
+
+export default {
+  getAllRoles,
+  createRole,
+  updateRole,
+  deleteRole,
+  deleteAllRole,
+};
