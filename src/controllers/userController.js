0133ea717a2f73eb886733a1234aa71177ea472d
Randy-RM <randymuhema@gmail.com>
2024-03-10 15:15:17 +0100

    feat : creat user controller and his basic CRUD function definition

---
+++ src/controllers/userController.js
@@ -0,0 +1,78 @@
+/*
+--------------------------
+Retrieve one user from 
+the database.
+--------------------------
+*/
+async function getOneUser(req, res, next) {
+  return res.send("One user");
+}
+
+/*
+--------------------------
+Retrieve all users from 
+the database.
+--------------------------
+*/
+async function getAllUsers(req, res, next) {
+  return res.send("All users");
+}
+
+/*
+--------------------------
+Create and save a new user
+in the database
+--------------------------
+*/
+async function createUser(req, res, next) {
+  return res.send("User is Created");
+}
+
+/*
+--------------------------
+Update a user by the id 
+in the request
+--------------------------
+*/
+async function updateUser(req, res, next) {
+  return res.send("User is updated");
+}
+
+/*
+--------------------------
+Delete a user with 
+the specified id 
+in the request
+--------------------------
+*/
+async function deleteUser(req, res, next) {
+  return res.send("User is deleted");
+}
+
+/*
+--------------------------
+Delete all users from 
+the database.
+--------------------------
+*/
+async function deleteAllUser(req, res, next) {
+  return res.send("Users are deleted");
+}
+
+export {
+  createUser,
+  deleteAllUser,
+  deleteUser,
+  getAllUsers,
+  getOneUser,
+  updateUser,
+};
+
+export default {
+  createUser,
+  deleteAllUser,
+  deleteUser,
+  getAllUsers,
+  getOneUser,
+  updateUser,
+};
