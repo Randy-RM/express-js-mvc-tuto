be15dc0f5e99ba18309525b9a1518f5b1e3b0e85
Randy-RM <randymuhema@gmail.com>
2024-03-13 14:58:46 +0100

    feat : create signup and signin function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,37 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model.js");
+
 /*
 --------------------------
 Create and save a new user
 in the database
 --------------------------
 */
-async function signup(req, res, next) {
-  return res.send("User is created");
+async function signup(req, res) {
+  const { username, email, password } = req.body;
+
+  const user = new UserModel({
+    username: username,
+    email: email,
+    password: await bcrypt.hash(password, 10),
+  });
+
+  try {
+    const newUser = await user
+      .save()
+      .then((user) => user)
+      .catch((error) => {
+        return { error: error.message };
+      });
+
+    if (newUser.error) {
+      throw new Error(newUser.error);
+    }
+    return res.status(201).json({ message: "User is created", data: newUser });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 /*
@@ -13,7 +39,7 @@ async function signup(req, res, next) {
 Activate user account
 --------------------------
 */
-function activateAccount(req, res, next) {
+function activateAccount(req, res) {
   return res.send("User account is activated");
 }
 
@@ -23,8 +49,35 @@ Signin if user have an account
 and roles 
 --------------------------
 */
-async function signin(req, res, next) {
-  return res.send("User is signin");
+async function signin(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const passwordMatch = await user.isValidPassword(password);
+    if (!passwordMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const token = jwt.sign(
+      { username: user.username, email: user.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.json({
+      message: "Logged in successfully",
+      user: { username: user.username, email: user.email },
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 /*
@@ -32,7 +85,7 @@ async function signin(req, res, next) {
 Logout if user is logged 
 --------------------------
 */
-async function logout(req, res, next) {
+async function logout(req, res) {
   return res.send("User is logout");
 }
 
@@ -41,7 +94,7 @@ async function logout(req, res, next) {
 Recover user account 
 --------------------------
 */
-async function recoverAccount(req, res, next) {
+async function recoverAccount(req, res) {
   return res.send("User account is recovered");
 }
 
@@ -50,7 +103,7 @@ async function recoverAccount(req, res, next) {
 Delete user account 
 --------------------------
 */
-async function deleteAccount(req, res, next) {
+async function deleteAccount(req, res) {
   return res.send("User account is deleted");
 }
 
