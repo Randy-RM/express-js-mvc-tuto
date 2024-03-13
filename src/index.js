cd1f0fb5523f8f1d88cbf15a3d4c61a3e840fbcc
Randy-RM <randymuhema@gmail.com>
2024-03-13 15:14:58 +0100

    feat : add session, passport auth and mongo database configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+const passport = require("passport");
+const dbConnection = require("./config/database");
+
 const {
   articlesBaseURI,
   authBaseURI,
@@ -15,14 +20,16 @@ const {
   userRouter,
 } = require("./routes");
 
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
 const app = express();
 const PORT = process.env.PORT || 8000;
 const corsOptions = {
   origin: `http://localhost:${PORT}`,
 };
-
-// Config
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header(
@@ -32,19 +39,56 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.MONGOHQ_URL,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    },
+  })
+);
+
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+// Need to require the entire Passport config module so app.js knows about it
+require("./config/passport")(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+/**
+ * -------------- ROUTE ----------------
+ */
 app.use(authBaseURI, authRouter);
 app.use(rolesBaseURI, roleRouter);
 app.use(usersBaseURI, userRouter);
 app.use(articlesBaseURI, articleRouter);
 
-// Run server
-mongoose
-  .connect(process.env.MONGOHQ_URL)
-  .then(() => {
-    console.log("Successful connection to DB");
-    app.listen(PORT, () => {
-      console.log(`The server listens on http://localhost:${PORT}`);
-    });
-  })
-  .catch(() => console.log("DB connection error"));
+/**
+ * -------------- RUN SERVER ----------------
+ */
+if (dbConnection) {
+  console.log("Successful connection to DB");
+  app.listen(PORT, () => {
+    console.log(`The server listens on http://localhost:${PORT}`);
+  });
+} else {
+  console.log("DB connection error");
+}
