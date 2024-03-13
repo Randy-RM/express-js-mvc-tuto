67ec75d445803d712bb569e58ceb382b851a0fa6
Randy-RM <randymuhema@gmail.com>
2024-03-13 14:42:31 +0100

    feat : creat user model for interaction with database

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+const { Schema } = require("mongoose");
+const dbConnection = require("../config/database");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+const UserModel = dbConnection.model("user", UserSchema);
+
+module.exports = UserModel;
