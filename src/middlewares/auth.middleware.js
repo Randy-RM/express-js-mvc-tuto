47a74d16694a700f4de89c6168addb646b80c193
Randy-RM <randymuhema@gmail.com>
2024-03-14 16:09:23 +0100

    feat : create admin role authorization middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,11 +9,12 @@ const isAuth = (req, res, next) => {
 };
 
 const isAdmin = (req, res, next) => {
-  if (req.isAuthenticated() && req.user.admin) {
+  if (req.user.role.roleName === "admin") {
     next();
   } else {
     res.status(401).json({
-      msg: "You are not authorized to view this resource because you are not an admin.",
+      message:
+        "You are not authorized to view this resource because you are not an admin.",
     });
   }
 };
