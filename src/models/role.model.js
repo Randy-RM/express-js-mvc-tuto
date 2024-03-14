0c06fbaf825e1aaca2b0b58b96da8e1f159f1059
Randy-RM <randymuhema@gmail.com>
2024-03-14 13:01:35 +0100

    feat : creat role model for interaction with database

---
+++ src/models/role.model.js
@@ -0,0 +1,18 @@
+const { Schema } = require("mongoose");
+const dbConnection = require("../config/database");
+
+const RoleSchema = new Schema(
+  {
+    roleName: {
+      type: String,
+      required: [true, "Please enter role name"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RoleModel = dbConnection.model("Role", RoleSchema);
+
+module.exports = RoleModel;
