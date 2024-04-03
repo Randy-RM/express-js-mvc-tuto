14ca530cb99a696d5ad976c310f8e17da2ffe93d
Randy-RM <randymuhema@gmail.com>
2024-04-03 13:58:55 +0200

    feat : add activateAccountURI on paths

--- src/config/paths.config.js
+++ src/config/paths.config.js
@@ -2,12 +2,14 @@ const authBaseURI = "/api/auth";
 const userBaseURI = "/api/user";
 const roleBaseURI = "/api/role";
 const articleBaseURI = "/api/article";
+const activateAccountURI = "/activate-account";
 
 const paths = {
   authBaseURI: authBaseURI,
   userBaseURI: userBaseURI,
   roleBaseURI: roleBaseURI,
   articleBaseURI: articleBaseURI,
+  activateAccountURI: activateAccountURI,
 };
 
 module.exports = paths;
