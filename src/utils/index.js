ae93b62edd4684ef34af08763982d7c1e1daa98a
Randy-RM <randymuhema@gmail.com>
2024-04-03 10:29:02 +0200

    feat : create send account activation email function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 function isUserAuthorizedToModifyResource({
   userIdInResource,
   logedUserId,
@@ -21,7 +23,42 @@ function randomStringGenerator() {
   return randomString;
 }
 
+function sendAccountActivationEmail(email, uniqueString) {
+  // on production env get this kind of config
+  // const transport = nodemailer.createTransport({
+  //   service: 'Gmail',
+  //   auth: {
+  //     user: 'may@gmail.com',
+  //     pass: 'myPassWord',
+  //   },
+  // });
+
+  const transport = nodemailer.createTransport({
+    service: "mailhog",
+    port: 1025,
+  });
+
+  const sender = "MiniBlog My Company <info@mini-blog.org>";
+  const mailOptions = {
+    from: sender,
+    to: email,
+    subject: "Email confirmation",
+    html: `Press <a href='${process.env.API_VERIFY}${uniqueString}'>Here</a> to verify your email.`,
+  };
+
+  transport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.log(error);
+      return { status: false, data: error };
+    } else {
+      console.log("Message sent");
+      return { status: true, data: response };
+    }
+  });
+}
+
 module.exports = {
   isUserAuthorizedToModifyResource: isUserAuthorizedToModifyResource,
   randomStringGenerator: randomStringGenerator,
+  sendAccountActivationEmail: sendAccountActivationEmail,
 };
