55cbc947349f50c20f6f0324f6d6ec78c18fd161
Randy-RM <randymuhema@gmail.com>
2024-05-22 14:49:13 +0200

    refactor : change articles resources path

--- src/routes/articles.routes.js
+++ src/routes/articles.routes.js
@@ -16,35 +16,35 @@ const {
 const articleRouter = Router();
 
 //Get all articles
-articleRouter.get(`/all`, getAllArticles);
+articleRouter.get(`/`, getAllArticles);
 
 //Get one article by articleId
 articleRouter.get(`/:articleId`, getOneArticle);
 
 //Create a new article
 articleRouter.post(
-  `/add`,
+  `/`,
   [passport.authenticate("jwt", { session: false }), isAuthorOrAdmin],
   createArticle
 );
 
 //Update article by articleId
 articleRouter.put(
-  `/update/:articleId`,
+  `/:articleId`,
   [passport.authenticate("jwt", { session: false }), isAuthorOrAdmin],
   updateArticle
 );
 
 //Delete article by articleId
 articleRouter.delete(
-  `/delete/:articleId`,
+  `/:articleId`,
   [passport.authenticate("jwt", { session: false }), isAuthorOrAdmin],
   deleteArticle
 );
 
 //Delete all articles
 articleRouter.delete(
-  `/delete`,
+  `/`,
   [passport.authenticate("jwt", { session: false }), isAdmin],
   deleteAllArticles
 );
