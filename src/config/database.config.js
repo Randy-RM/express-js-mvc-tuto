bce7264f3f3317de5de2520030edc062e9b68559
Randy-RM <randymuhema@gmail.com>
2024-05-22 13:39:48 +0200

    refactor : refactor DB connection

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const dbString = process.env.MONGOHQ_URL;
 const dbOptions = {};
-let dbConnection = {};
-try {
-  dbConnection = mongoose.createConnection(dbString, dbOptions);
-} catch (error) {
-  dbConnection = null;
-}
+const dbConnection = mongoose.createConnection(dbString, dbOptions);
 // Expose the connection
 module.exports = dbConnection;
