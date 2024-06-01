d585da6986a07300bfbc31684c9848f2baf5a668
Randy-RM <randymuhema@gmail.com>
2024-06-01 10:48:34 +0200

    refactor : refactor router system

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,17 @@
-module.exports = {
-  articleRouter: require("./articles.routes.js"),
-  authRouter: require("./auth.routes.js"),
-  roleRouter: require("./roles.routes.js"),
-  userRouter: require("./users.routes.js"),
-};
+const { Router } = require("express");
+
+const {
+  articleBaseURI,
+  authBaseURI,
+  roleBaseURI,
+  userBaseURI,
+} = require("../config/paths.config");
+
+const router = Router();
+
+router.use(authBaseURI, require("./auth.routes.js"));
+router.use(roleBaseURI, require("./roles.routes.js"));
+router.use(userBaseURI, require("./users.routes.js"));
+router.use(articleBaseURI, require("./articles.routes.js"));
+
+module.exports = router;
