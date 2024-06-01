6ee8c450c377923bec69dfb9fc65d7d101f31441
Randy-RM <randymuhema@gmail.com>
2024-06-01 10:43:45 +0200

    feat : add authorize middleware function for authorization system

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,49 +8,51 @@ const {
   getOneUser,
   updateUser,
 } = require("../controllers/user.controller.js");
-const { isAdmin } = require("../middlewares/auth.middleware.js");
+const { authorize } = require("../middlewares/auth.middleware.js");
+
+const { ROLES } = require("../constant");
 
 const userRouter = Router();
 
 //Get all users
 userRouter.get(
   `/`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   getAllUsers
 );
 
 //Get one user by userId
 userRouter.get(
   `/:userId`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false })],
   getOneUser
 );
 
 //Create a new user
 userRouter.post(
   `/`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   createUser
 );
 
 //Update user by userId
 userRouter.put(
   `/:userId`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false })],
   updateUser
 );
 
 //Delete user by userId
 userRouter.delete(
   `/:userId`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false })],
   deleteUser
 );
 
 //Delete all users
 userRouter.delete(
   `/`,
-  [passport.authenticate("jwt", { session: false }), isAdmin],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   deleteAllUsers
 );
 
