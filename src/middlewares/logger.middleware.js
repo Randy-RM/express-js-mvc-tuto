172383d3f86c0f813eb50b0fc7a77f22d87e7cd3
Randy-RM <randymuhema@gmail.com>
2024-06-01 12:52:14 +0200

    feat : create logger function for display colorful logs on console

---
+++ src/middlewares/logger.middleware.js
@@ -0,0 +1,20 @@
+const colors = require("colors");
+
+function logger(req, res, next) {
+  const methodColor = {
+    GET: "green",
+    POST: "blue",
+    PUT: "yellow",
+    DELETE: "red",
+  };
+
+  const color = methodColor[req.method] || "white";
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`[
+      color
+    ]
+  );
+  next();
+}
+
+module.exports = logger;
