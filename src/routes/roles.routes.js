c7d70e363ad29bfe509bd39c06e25540c24fa6cb
Randy-RM <randymuhema@gmail.com>
2024-06-01 10:42:26 +0200

    feat : add authorize middleware function for authorization system

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -8,48 +8,51 @@ const {
   getOneRole,
   updateRole,
 } = require("../controllers/role.controller.js");
+const { authorize } = require("../middlewares/auth.middleware.js");
+
+const { ROLES } = require("../constant");
 
 const roleRouter = Router();
 
 //Get all roles
 roleRouter.get(
   `/`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   getAllRoles
 );
 
 //Get one role by roleId
 roleRouter.get(
   `/:roleId`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   getOneRole
 );
 
 //Create a new role
 roleRouter.post(
   `/`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   createRole
 );
 
 //Update role by roleId
 roleRouter.put(
   `/:roleId`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   updateRole
 );
 
 //Delete role by roleId
 roleRouter.delete(
   `/:roleId`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   deleteRole
 );
 
 //Delete all roles
 roleRouter.delete(
   `/`,
-  [passport.authenticate("jwt", { session: false })],
+  [passport.authenticate("jwt", { session: false }), authorize([ROLES.ADMIN])],
   deleteAllRoles
 );
 
