4c3b34c3e41a768450b876c4ec18204afeb62977
Randy-RM <randymuhema@gmail.com>
2024-06-01 11:11:57 +0200

    refactor : remove unused middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -15,31 +15,4 @@ function authorize(roles) {
   };
 }
 
-const isAuthorOrAdmin = (req, res, next) => {
-  if (
-    req.user.role.roleName === "author" ||
-    req.user.role.roleName === "admin"
-  ) {
-    next();
-  } else {
-    res.status(401).json({
-      message:
-        "You are not authorized to view this resource because you are not an author or admin.",
-      data: {},
-    });
-  }
-};
-
-const isAdmin = (req, res, next) => {
-  if (req.user.role.roleName === "admin") {
-    next();
-  } else {
-    res.status(401).json({
-      message:
-        "You are not authorized to view this resource because you are not an admin.",
-      data: {},
-    });
-  }
-};
-
-module.exports = { authorize, isAuthorOrAdmin, isAdmin };
+module.exports = { authorize };
