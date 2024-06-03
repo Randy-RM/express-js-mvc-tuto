69fdaa0922db37d21046ce5e0c90ed0bed59b6b2
Randy-RM <randymuhema@gmail.com>
2024-06-03 14:22:32 +0200

    refactor : add control structur for null return on getOneUser controller function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,6 +13,13 @@ async function getOneUser(req, res) {
 
   try {
     const user = await UserModel.findById(userId).populate("role");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: `User with id "${userId}" not found` });
+    }
+
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
