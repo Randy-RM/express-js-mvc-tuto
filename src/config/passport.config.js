49eb5b96266ec551e94a1f1584fc4fbbe44d95ee
Randy-RM <randymuhema@gmail.com>
2024-11-14 15:04:29 +0000

    refactor : refactor passport config logic

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -2,17 +2,17 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const UserModel = require("../models/user.model");
 
-const opts = {
+const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
 };
 
 module.exports = (passport) => {
   passport.use(
-    new JwtStrategy(opts, async (jwt_payload, done) => {
+    new JwtStrategy(options, async (jwt_payload, done) => {
       const user = await UserModel.findOne({
         email: jwt_payload.email,
-      }).populate("role");
+      });
 
       if (user) {
         return done(null, user);
