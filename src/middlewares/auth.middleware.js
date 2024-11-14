1031d204e71c9b583c3429e9cb9326138921a57d
Randy-RM <randymuhema@gmail.com>
2024-11-14 15:06:46 +0000

    refactor : refactor authorize middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,12 @@
 function authorize(roles) {
   return (req, res, next) => {
-    const {
-      role: { roleName },
-    } = req.user;
+    const { role } = req.user;
 
-    if (roles.includes(roleName)) {
+    if (roles.includes(role)) {
       next(); // User is authorized, proceed to the next middleware or route handler
     } else {
       return res.status(403).json({
-        message:
-          "You are not authorize to perform this action because of your role",
+        message: "You are not authorize to perform this action",
       });
     }
   };
