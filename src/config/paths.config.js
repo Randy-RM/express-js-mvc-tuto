0550875e25b544ab739de74ebd029c0ad8e921a8
Randy-RM <randymuhema@gmail.com>
2024-11-14 14:52:06 +0000

    refactor : remove role base url path route

--- src/config/paths.config.js
+++ src/config/paths.config.js
@@ -1,13 +1,11 @@
 const authBaseURI = "/auth";
 const userBaseURI = "/users";
-const roleBaseURI = "/roles";
 const articleBaseURI = "/articles";
 const activateAccountURI = "/activate-account";
 
 const paths = {
   authBaseURI: authBaseURI,
   userBaseURI: userBaseURI,
-  roleBaseURI: roleBaseURI,
   articleBaseURI: articleBaseURI,
   activateAccountURI: activateAccountURI,
 };
