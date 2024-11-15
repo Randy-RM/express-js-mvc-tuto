66c02300e6869e044b6e4ce49776c9eb50e3e7b7
Randy-RM <randymuhema@gmail.com>
2024-11-15 08:34:48 +0000

    refactor : remove unused role routes path

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,12 @@ const { Router } = require("express");
 const {
   articleBaseURI,
   authBaseURI,
-  roleBaseURI,
   userBaseURI,
 } = require("../config/paths.config");
 
 const router = Router();
 
 router.use(authBaseURI, require("./auth.routes.js"));
-router.use(roleBaseURI, require("./roles.routes.js"));
 router.use(userBaseURI, require("./users.routes.js"));
 router.use(articleBaseURI, require("./articles.routes.js"));
 
