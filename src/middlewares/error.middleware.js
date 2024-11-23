4bf0f358d3017e3e1bc76bd299192c8a4ff4d2c7
Randy-RM <randymuhema@gmail.com>
2024-11-23 11:40:17 +0000

    refactor : refactor errorHandler middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ function errorHandler(err, req, res, next) {
   console.log("Middleware Error Hadnling");
   const errStatus = err.statusCode || 500;
   const errMsg = err.message || "Something went wrong";
-  res.status(errStatus).json({
+  return res.status(errStatus).json({
     success: false,
     status: errStatus,
     message: errMsg,
